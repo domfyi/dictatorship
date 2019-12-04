006b3fea3a8200b4e90ac93586002574b9473f67
Dom <domvinyard@me.com>
2019-12-04 01:20:17 +0000

    PMs

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,97 @@
 <script>
-	export let name;
+  import { majorities, acts, pms } from "./data.js";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    max-width: 700px;
+    margin: 0 auto;
+    background: #fff;
+    overflow: hidden;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  header {
+    height: 250vh;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  header p {
+    margin: 0;
+    padding: 0;
+    padding: 0 5vw;
+    font-size: 1.4rem;
+    color: #444;
+    line-height: 2rem;
+  }
+
+  header p strong {
+    border-bottom: 2px solid #333;
+    padding-bottom: 1px;
+  }
+
+  h1 {
+    background: url("/union-jack.jpg");
+    background-size: cover;
+    background-position: center center;
+    text-transform: uppercase;
+    font-size: 2.6rem;
+    font-weight: 200;
+    padding: 1.5rem 2rem;
+    border-top: 2px solid #4c4c4c;
+    border-bottom: 2px solid #4c4c4c;
+    margin-left: -20px;
+    margin-right: -20px;
+  }
+  h1,
+  header p {
+    transform: rotate(-2.2deg);
+  }
+  h1 div:first-child {
+    font-size: 3rem;
+    border-bottom: 2px solid #222;
+    width: 380px;
+    margin: 0 auto;
+  }
+  section.history {
+    height: 200vh;
+    background: #eee;
+  }
+
+  .cover {
+    background: #b3b3b3;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .cover-title {
+    width: 100%;
+    height: 66vh;
+  }
 </style>
+
+<main>
+  <header>
+    <div class="cover">
+      <div class="cover-title">
+        <p>The UK has been described as an</p>
+        <h1>
+          <div>Elected</div>
+          <div>Dictatorship</div>
+        </h1>
+        <p>
+          Because a government with a big enough
+          <strong>majority</strong>
+          can essentially do what it wants
+        </p>
+      </div>
+    </div>
+  </header>
+  <section class="history">
+    <div>history</div>
+    <div>history</div>
+    <div>history</div>
+    <div>history</div>
+    <div>history</div>
+  </section>
+</main>
---
+++ src/data.js
@@ -0,0 +1,214 @@
+export const majorities = [];
+
+export const acts = [
+  {
+    date: "2019-06-01",
+    name: "Test act",
+    description: "is a test act",
+    link: "https://gov.uk/test"
+  },
+  {
+    date: "2019-10-01",
+    party: "CON",
+    majority: 10
+  }
+];
+
+export const pms = [
+  {
+    date: "2019-07-24",
+    name: "Boris Johnson",
+    nickname: "Boris",
+    party: "CON",
+    image: "Johnson.png"
+  },
+  {
+    date: "2016-07-13",
+    name: "Theresa May",
+    nickname: "May",
+    party: "CON",
+    image: "May.png"
+  },
+  {
+    date: "2010-05-11",
+    name: "David Cameron",
+    nickname: "Cameron",
+    party: "CON",
+    image: "Cameron.png"
+  },
+  {
+    date: "2007-06-27",
+    name: "Gordon Brown",
+    nickname: "Brown",
+    party: "LAB",
+    image: "Brown.png"
+  },
+  {
+    date: "1997-05-02",
+    name: "Tony Blair",
+    nickname: "Blair",
+    party: "LAB",
+    image: "Blair.png"
+  },
+  {
+    date: "1990-11-28",
+    name: "John Major",
+    nickname: "Major",
+    party: "CON",
+    image: "Major.png"
+  },
+  {
+    date: "1979-05-04",
+    name: "Margeret Thatcher",
+    nickname: "Maggie",
+    party: "CON",
+    image: "Thatcher.png"
+  },
+  {
+    date: "1976-04-05",
+    name: "James Callaghan",
+    nickname: "Callaghan",
+    party: "LAB",
+    image: "Callaghan.png"
+  },
+  {
+    date: "1974-03-04",
+    name: "Harold Wilson",
+    nickname: "Wilson",
+    party: "LAB",
+    image: "Wilson.png"
+  },
+  {
+    date: "1970-06-19",
+    name: "Edward Heath",
+    nickname: "Heath",
+    party: "CON",
+    image: "Heath.png"
+  },
+  {
+    date: "1964-10-16",
+    name: "Harold Wilson",
+    nickname: "Wilson",
+    party: "LAB",
+    image: "Wilson.png"
+  },
+  {
+    date: "1963-10-19",
+    name: "Alec Douglas-Home",
+    nickname: "Home",
+    party: "CON",
+    image: "Home.png"
+  },
+  {
+    date: "1957-01-10",
+    name: "Harold Macmillan",
+    nickname: "Macmillan",
+    party: "CON",
+    image: "Macmillan.png"
+  },
+  {
+    date: "1955-04-06",
+    name: "Anthony Eden",
+    nickname: "Eden",
+    party: "CON",
+    image: "Eden.png"
+  },
+  {
+    date: "1951-10-26",
+    name: "Winston Churchill",
+    nickname: "Churchill",
+    party: "CON",
+    image: "Churchill.png"
+  },
+  {
+    date: "1945-07-26",
+    name: "Clement Attlee",
+    nickname: "Atlee",
+    party: "LAB",
+    image: "Atlee.png"
+  },
+  {
+    date: "1940-05-10",
+    name: "Winston Churchill",
+    nickname: "Churchill",
+    party: "CON",
+    image: "Churchill.png"
+  },
+  {
+    date: "1937-05-28",
+    name: "Neville Chamberlain",
+    nickname: "Chamberlain",
+    party: "CON",
+    image: "Chamberlain.png"
+  },
+  {
+    date: "1935-06-07",
+    name: "Stanley Baldwin",
+    nickname: "Baldwin",
+    party: "CON",
+    image: "Baldwin.png"
+  },
+  {
+    date: "1929-06-05",
+    name: "Ramsay MacDonald",
+    nickname: "MacDonald",
+    party: "LAB",
+    image: "MacDonald.png"
+  },
+  {
+    date: "1924-11-04",
+    name: "Stanley Baldwin",
+    nickname: "Baldwin",
+    party: "CON",
+    image: "Baldwin.png"
+  },
+  {
+    date: "1924-01-22",
+    name: "Ramsay MacDonald",
+    nickname: "MacDonald",
+    party: "LAB",
+    image: "MacDonald.png"
+  },
+  {
+    date: "1923-05-22",
+    name: "Stanley Baldwin",
+    nickname: "Baldwin",
+    party: "CON",
+    image: "Baldwin.png"
+  },
+  {
+    date: "1922-10-23",
+    name: "Bonar Law",
+    nickname: "Bonar Law",
+    party: "CON",
+    image: "MacDonald.png"
+  },
+  {
+    date: "1916-12-06",
+    name: "David Lloyd George",
+    nickname: "Lloyd George",
+    party: "LIB",
+    image: "Lloyd-George.png"
+  },
+  {
+    date: "1908-04-08",
+    name: "H. H. Asquith",
+    nickname: "Asquith",
+    party: "LIB",
+    image: "Asquith.png"
+  },
+  {
+    date: "1905-12-05",
+    name: "Henry Campbell-Bannerman",
+    nickname: "Bannerman",
+    party: "LIB",
+    image: "Campbell-Bannerman.png"
+  },
+  {
+    date: "1902-07-12",
+    name: "Arthur Balfour",
+    nickname: "Balfour",
+    party: "CON",
+    image: "Balfour.png"
+  }
+];
