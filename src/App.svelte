f59ebdba7aefbe7d2bb3091134b5ac6401f8f49d
Dom <domvinyard@me.com>
2019-12-05 00:50:35 +0000

    header

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,85 @@
 <script>
-  import { majorities, acts, pms } from "./data.js";
+  import { pms, parties } from "./data.js";
+  const [pm1, pm2, pm3] = pms;
+  let y;
+  let height;
+  $: animations = {
+    cover: {
+      startFade: (height / 4) * 0
+    },
+    pm2: {
+      up: 10,
+      down: 60
+    },
+    pm3: {
+      up: 30,
+      down: 80
+    },
+    pms: {
+      up1: 100,
+      pause: 550,
+      up2: 950,
+      stop: 1100
+    }
+  };
+
+  const setpmsBottom = () => {
+    const { up1, pause, up2, stop } = animations.pms;
+    const pmHeight = height / 3;
+    const startPoint = 0;
+    const midPoint = height - height / 2 - pmHeight / 2;
+    const endPoint = height - pmHeight;
+    const firstHalfDistance = midPoint - startPoint;
+    const firstHalfTime = pause - up1;
+    const secondHalfDistance = endPoint - midPoint;
+    const secondHalfTime = stop - up2;
+    const timeDistanceToPixels = (t, d) => d / t;
+    const firstHalfPxPerPx = timeDistanceToPixels(
+      firstHalfTime,
+      firstHalfDistance
+    );
+    const secondHalfPxPerPx = timeDistanceToPixels(
+      secondHalfTime,
+      secondHalfDistance
+    );
+    const pmBottom =
+      y > up1 && y <= pause
+        ? (y - up1) * firstHalfPxPerPx
+        : y > pause && y <= up2
+        ? midPoint
+        : y > up2 && y <= stop
+        ? midPoint + (y - up2) * secondHalfPxPerPx
+        : y > stop
+        ? endPoint
+        : startPoint;
+    const rounded = Math.round(pmBottom);
+    console.log(rounded);
+    return rounded;
+  };
+  $: pmsBottom = setpmsBottom(y, height);
 </script>
 
 <style>
+  main,
+  .overlay {
+    max-width: 700px;
+  }
   main {
     text-align: center;
-    max-width: 700px;
     margin: 0 auto;
-    background: #fff;
     overflow: hidden;
+  }
+  .main-inner {
+    background: #fff;
+  }
+  .main-inner,
+  .overlay-inner {
+    margin: 0 auto;
   }
 
   header {
-    height: 250vh;
+    height: 100vh;
+    padding-bottom: 800px;
   }
 
   header p {
@@ -20,8 +87,16 @@
     padding: 0;
     padding: 0 5vw;
     font-size: 1.4rem;
-    color: #444;
+    color: #222;
     line-height: 2rem;
+  }
+
+  p .citation {
+    font-size: 0.9rem;
+    opacity: 0.66;
+    text-align: right;
+    display: block;
+    font-weight: 200;
   }
 
   header p strong {
@@ -29,69 +104,266 @@
     padding-bottom: 1px;
   }
 
+  h1,
+  .majority-text {
+    /* font-family: "Amatic SC", cursive; */
+    font-family: "Big Shoulders Display";
+    /* font-family: "Big Shoulders Display", cursive; */
+    /* font-family: "Zilla Slab Highlight", cursive; */
+    /* font-family: "Single Day"; */
+  }
+
   h1 {
-    background: url("/union-jack.jpg");
+    background: url("https://thumbs.gfycat.com/PastelCloudyGelding-size_restricted.gif");
     background-size: cover;
     background-position: center center;
     text-transform: uppercase;
-    font-size: 2.6rem;
-    font-weight: 200;
-    padding: 1.5rem 2rem;
-    border-top: 2px solid #4c4c4c;
-    border-bottom: 2px solid #4c4c4c;
+    font-size: 3rem;
+    /* font-weight: 300; */
+    padding: 4rem 2rem;
+    border-top: 3px solid #222;
+    border-bottom: 3px solid #222;
     margin-left: -20px;
     margin-right: -20px;
+    color: #fff;
+    letter-spacing: 1px;
   }
   h1,
   header p {
-    transform: rotate(-2.2deg);
+    transform: rotate(-2.6deg);
+  }
+  header p:first-child {
+    font-size: 1.5rem;
   }
   h1 div:first-child {
-    font-size: 3rem;
-    border-bottom: 2px solid #222;
+    font-size: 4rem;
+    line-height: 4rem;
+    border-bottom: 2px solid #eee;
     width: 380px;
     margin: 0 auto;
   }
   section.history {
-    height: 200vh;
-    background: #eee;
   }
 
   .cover {
-    background: #b3b3b3;
-    height: 100vh;
+    height: 110vh;
+  }
+
+  .cover-inner {
+    height: 75vh;
     display: flex;
     align-items: center;
     justify-content: center;
   }
   .cover-title {
     width: 100%;
-    height: 66vh;
+  }
+  .overlay {
+    text-align: center;
+    height: 33vh;
+    z-index: 10;
+    left: 50%;
+    transform: translateX(-50%);
+    overflow: hidden;
+    border-bottom: 6px solid #222;
+    margin-bottom: -6px;
+  }
+  .majority,
+  .overlay {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+  .pms {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    vertical-align: bottom;
+    height: 66%;
+  }
+  .pms > div {
+    height: 100%;
+    vertical-align: bottom;
+    height: 100%;
+  }
+  .pms img {
+    width: auto;
+    position: absolute;
+    bottom: 0px;
+    margin-bottom: -10px;
+  }
+  .pm3 {
+    margin-left: -25vh;
+    z-index: 1;
+    height: 80%;
+    /* transition: 0.75s all; */
+    animation-duration: 0.8s;
+
+    margin-bottom: -22px !important;
+    filter: brightness(60%);
+  }
+  .pm1 {
+    z-index: 3;
+    height: 100%;
+    transition: 0.8s all;
+  }
+  .pm1.left {
+    margin-left: -33%;
+  }
+  .pm2 {
+    margin-left: 25vh;
+    z-index: 2;
+    height: 90%;
+    animation-duration: 0.8s;
+    animation-delay: 0.15s;
+    margin-bottom: -22px !important;
+    filter: brightness(75%);
+  }
+  /* .pms img.up {
+    margin-bottom: -1px;
+  }
+  .pms img.down {
+    margin-bottom: -33vh;
+  } */
+  .scroll-down {
+    color: white;
+    z-index: 4;
+    opacity: 1;
+    position: fixed;
+    bottom: 0;
+    width: 20px;
+    height: 20px;
+    border-radius: 100%;
+    z-index: 4;
+    margin-left: -10px;
+    margin-bottom: 4px;
+    background: rgba(107, 107, 107, 0.75);
+    color: black;
+    font-size: 2rem;
+    display: none;
+  }
+  .majority {
+    height: 33% !important;
+    background: red;
+    width: 100%;
+    line-height: 12vh;
+    font-size: 8vh;
+    font-weight: bold;
+    letter-spacing: 1px;
+    color: #fff;
+    text-align: right;
+  }
+  .majority-text {
+    margin-right: 1rem;
+    display: inline-block;
+    color: #222;
+    text-transform: uppercase;
+  }
+  .mini-pm-container {
+    margin-top: 100px;
+    border-bottom: 1px solid #333;
+  }
+  .mini-pm {
+    height: 100px;
+    filter: grayscale(0.66);
+    opacity: 0.75;
+    margin-bottom: -4px;
+    font-weight: 100;
+  }
+  .mini-pm-name {
+    padding-top: 4px;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    color: #444;
+  }
+  .mini-pm-date {
+    letter-spacing: 1px;
+    color: #888;
+    font-size: 0.8rem;
+    border-top: 1px solid #d1d1d1;
+    margin-top: 2px;
+    padding: 2px 4px 0 4px;
+    display: inline-block;
+    margin-bottom: 200px;
+  }
+  .sic {
+    opacity: 0.66;
   }
 </style>
 
+<svelte:window bind:scrollY={y} bind:innerHeight={height} />
 <main>
-  <header>
-    <div class="cover">
-      <div class="cover-title">
-        <p>The UK has been described as an</p>
-        <h1>
-          <div>Elected</div>
-          <div>Dictatorship</div>
-        </h1>
-        <p>
-          Because a government with a big enough
-          <strong>majority</strong>
-          can essentially do what it wants
-        </p>
+  <div class="main-inner">
+    <header>
+
+      <div
+        class="cover"
+        style={`
+		background-image: linear-gradient(
+			rgba(179,179,179,1), 
+			rgba(179,179,179,${y < animations.cover.startFade ? 1 : 1 - (y - animations.cover.startFade) / 1000}),
+			rgba(179,179,179,${y < animations.cover.startFade ? 1 : 1 - (y - animations.cover.startFade) / 100})
+		);
+	  `}>
+        <div class="cover-inner">
+          <div
+            class="cover-title"
+            style={`margin-top: -${Math.min(y, 1000)}px`}>
+            <p>The UK has been described as an</p>
+            <h1>
+              <div>Elective</div>
+              <div>Dictatorship</div>
+            </h1>
+            <p>
+              Because a government elected with a big enough
+              <strong>majority</strong>
+              can essentially do what it wants.
+              <span class="citation">— Lord Hailsham</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div
+      class="overlay"
+      style={`bottom: ${pmsBottom}px; background: ${y > height ? '#fff' : 'none'}`}>
+      <div class="ovelay-inner">
+        <div class="pms">
+          <img
+            alt="pm2"
+            class={`pm2 ${y > animations.pm2.down ? 'bounceOutDown' : 'bounceInUp'} animated`}
+            src={`/pms/${pm2.image}`} />
+          <img
+            alt="pm1"
+            class={`pm1 ${y > animations.pms.pause ? 'left' : ''}`}
+            src={`/pms/${pm1.image}`} />
+          <img
+            alt="pm3"
+            class={`pm3 ${y > animations.pm3.down ? 'bounceOutDown' : 'bounceInUp'} animated`}
+            src={`/pms/${pm3.image}`} />
+        </div>
+        <div class={`majority`} style={`background: ${parties[pm1.party]}`}>
+          <span
+            class="majority-text"
+            style={`transform: translateY(${y < animations.pms.pause ? 100 : Math.max(0, 100 - (y - animations.pms.pause))}px)`}>
+            {pm1.nickname}
+          </span>
+        </div>
+        <span class="scroll-down" style="opacity: {1 - Math.max(0, y / 80)}">
+          ˅
+        </span>
       </div>
     </div>
-  </header>
-  <section class="history">
-    <div>history</div>
-    <div>history</div>
-    <div>history</div>
-    <div>history</div>
-    <div>history</div>
-  </section>
+    <section class="history">
+      {#each pms.slice(1) as pm}
+        <div class="mini-pm-container">
+          <img class="mini-pm" src={`/pms/${pm.image}`} />
+        </div>
+        <div class="mini-pm-name">{pm.nickname}</div>
+        <div class="mini-pm-date">
+          <span>{pm.date.slice(0, 4)}</span>
+        </div>
+      {/each}
+    </section>
+  </div>
 </main>
--- src/data.js
+++ src/data.js
@@ -1,18 +1,11 @@
-export const majorities = [];
+export const parties = {
+  CON: "rgb(0, 144, 235);",
+  LAB: "rgb(227, 7, 2)",
+  LIB: "rgb(232, 160, 0)"
+};
 
-export const acts = [
-  {
-    date: "2019-06-01",
-    name: "Test act",
-    description: "is a test act",
-    link: "https://gov.uk/test"
-  },
-  {
-    date: "2019-10-01",
-    party: "CON",
-    majority: 10
-  }
-];
+// inject acts into pms
+export const acts = [];
 
 export const pms = [
   {
@@ -20,195 +13,280 @@ export const pms = [
     name: "Boris Johnson",
     nickname: "Boris",
     party: "CON",
-    image: "Johnson.png"
+    image: "Johnson.png",
+    majority: [
+      {
+        date: "2019-07-24",
+        majority: -5,
+        seats: 650
+      }
+    ]
   },
   {
     date: "2016-07-13",
     name: "Theresa May",
-    nickname: "May",
+    nickname: "Theresa",
     party: "CON",
-    image: "May.png"
+    image: "May.png",
+    majority: [
+      {
+        date: "2016-07-13",
+        majority: 12,
+        seats: 650
+      }
+    ]
   },
   {
     date: "2010-05-11",
     name: "David Cameron",
     nickname: "Cameron",
     party: "CON",
-    image: "Cameron.png"
+    image: "Cameron.png",
+    majority: [
+      {
+        date: "2010-05-11",
+        majority: 78,
+        seats: 650
+      }
+    ]
   },
   {
     date: "2007-06-27",
     name: "Gordon Brown",
     nickname: "Brown",
     party: "LAB",
-    image: "Brown.png"
+    image: "Brown.png",
+    majority: [
+      {
+        date: "2007-06-27",
+        majority: 66,
+        seats: 646
+      }
+    ]
   },
   {
     date: "1997-05-02",
     name: "Tony Blair",
     nickname: "Blair",
     party: "LAB",
-    image: "Blair.png"
+    image: "Blair.png",
+    majority: [
+      {
+        date: "2001-06-07",
+        majority: 167,
+        seats: 659
+      },
+      {
+        date: "1997-05-02",
+        majority: 179,
+        seats: 659
+      }
+    ]
   },
   {
     date: "1990-11-28",
     name: "John Major",
     nickname: "Major",
     party: "CON",
-    image: "Major.png"
+    image: "Major.png",
+    majority: [
+      {
+        date: "1990-11-28",
+        majority: 21,
+        seats: 651
+      }
+    ]
   },
   {
     date: "1979-05-04",
     name: "Margeret Thatcher",
     nickname: "Maggie",
     party: "CON",
-    image: "Thatcher.png"
+    image: "Thatcher.png",
+    majority: [
+      {
+        date: "1987-06-11",
+        majority: 102,
+        seats: 650
+      },
+      {
+        date: "1983-06-09",
+        majority: 144,
+        seats: 650
+      },
+      {
+        date: "1979-05-03",
+        majority: 43,
+        seats: 635
+      }
+    ]
   },
   {
     date: "1976-04-05",
     name: "James Callaghan",
     nickname: "Callaghan",
     party: "LAB",
-    image: "Callaghan.png"
+    image: "Callaghan.png",
+    majority: []
   },
   {
     date: "1974-03-04",
     name: "Harold Wilson",
     nickname: "Wilson",
     party: "LAB",
-    image: "Wilson.png"
+    image: "Wilson.png",
+    majority: []
   },
   {
     date: "1970-06-19",
     name: "Edward Heath",
-    nickname: "Heath",
+    nickname: "Ed Heath",
     party: "CON",
-    image: "Heath.png"
+    image: "Heath.png",
+    majority: []
   },
   {
     date: "1964-10-16",
     name: "Harold Wilson",
     nickname: "Wilson",
     party: "LAB",
-    image: "Wilson.png"
+    image: "Wilson.png",
+    majority: []
   },
   {
     date: "1963-10-19",
     name: "Alec Douglas-Home",
-    nickname: "Home",
+    nickname: "Doug-Home",
     party: "CON",
-    image: "Home.png"
+    image: "Home.png",
+    majority: []
   },
   {
     date: "1957-01-10",
     name: "Harold Macmillan",
     nickname: "Macmillan",
     party: "CON",
-    image: "Macmillan.png"
+    image: "Macmillan.png",
+    majority: []
   },
   {
     date: "1955-04-06",
     name: "Anthony Eden",
     nickname: "Eden",
     party: "CON",
-    image: "Eden.png"
+    image: "Eden.png",
+    majority: []
   },
   {
     date: "1951-10-26",
     name: "Winston Churchill",
     nickname: "Churchill",
     party: "CON",
-    image: "Churchill.png"
+    image: "Churchill.png",
+    majority: []
   },
   {
     date: "1945-07-26",
     name: "Clement Attlee",
     nickname: "Atlee",
     party: "LAB",
-    image: "Atlee.png"
+    image: "Atlee.png",
+    majority: []
   },
   {
     date: "1940-05-10",
     name: "Winston Churchill",
     nickname: "Churchill",
     party: "CON",
-    image: "Churchill.png"
+    image: "Churchill.png",
+    majority: []
   },
   {
     date: "1937-05-28",
     name: "Neville Chamberlain",
     nickname: "Chamberlain",
     party: "CON",
-    image: "Chamberlain.png"
+    image: "Chaimberlain.png",
+    majority: []
   },
   {
     date: "1935-06-07",
     name: "Stanley Baldwin",
     nickname: "Baldwin",
     party: "CON",
-    image: "Baldwin.png"
+    image: "Baldwin.png",
+    majority: []
   },
   {
     date: "1929-06-05",
     name: "Ramsay MacDonald",
     nickname: "MacDonald",
     party: "LAB",
-    image: "MacDonald.png"
+    image: "MacDonald.png",
+    majority: []
   },
   {
     date: "1924-11-04",
     name: "Stanley Baldwin",
     nickname: "Baldwin",
     party: "CON",
-    image: "Baldwin.png"
+    image: "Baldwin.png",
+    majority: []
   },
   {
     date: "1924-01-22",
     name: "Ramsay MacDonald",
     nickname: "MacDonald",
     party: "LAB",
-    image: "MacDonald.png"
+    image: "MacDonald.png",
+    majority: []
   },
   {
     date: "1923-05-22",
     name: "Stanley Baldwin",
     nickname: "Baldwin",
     party: "CON",
-    image: "Baldwin.png"
+    image: "Baldwin.png",
+    majority: []
   },
   {
     date: "1922-10-23",
     name: "Bonar Law",
     nickname: "Bonar Law",
     party: "CON",
-    image: "MacDonald.png"
+    image: "Bonar-Law.png",
+    majority: []
   },
   {
     date: "1916-12-06",
     name: "David Lloyd George",
     nickname: "Lloyd George",
     party: "LIB",
-    image: "Lloyd-George.png"
+    image: "Lloyd-George.png",
+    majority: []
   },
   {
     date: "1908-04-08",
     name: "H. H. Asquith",
     nickname: "Asquith",
     party: "LIB",
-    image: "Asquith.png"
+    image: "Asquith.png",
+    majority: []
   },
   {
     date: "1905-12-05",
     name: "Henry Campbell-Bannerman",
     nickname: "Bannerman",
     party: "LIB",
-    image: "Campbell-Bannerman.png"
+    image: "Campbell-Bannerman.png",
+    majority: []
   },
   {
     date: "1902-07-12",
     name: "Arthur Balfour",
     nickname: "Balfour",
     party: "CON",
-    image: "Balfour.png"
+    image: "Balfour.png",
+    majority: []
   }
 ];
